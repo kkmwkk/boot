1174d816c969a7b9f11493186b87485e3ba7f582
miw woo <mlolw2@naver.com>
2022-09-17 22:33:05 +0900

    yml create 수정, GException 수

--- blog/java/com/cos/blog/controller/api/UserApiController.java
+++ blog/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,20 +1,29 @@
 package com.cos.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
 
 @RestController
 public class UserApiController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@PostMapping("api/user")
-	public ResponseDto<Integer> save(@RequestBody User user) {
+	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨" );
-		return new ResponseDto<Integer>(HttpStatus.OK, 1); // 자바오브젝트를 JSON으로 변환 (Jackson)
+		// username, password, email 값은 받은대로 넣어주면 되는데 Role은 따로 넣어줘야되기 때문에
+		user.setRole(RoleType.USER);
+		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환 (Jackson)
 	}
 
 }
--- blog/java/com/cos/blog/model/User.java
+++ blog/java/com/cos/blog/model/User.java
@@ -30,7 +30,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)// 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
 	private int id; // 시퀀스, auto_increment
 	
-	@Column(nullable = false,length = 30)
+	@Column(unique = true, nullable = false,length = 30)
 	private String username; // 아이디
 	
 	@Column(nullable = false,length = 100)
