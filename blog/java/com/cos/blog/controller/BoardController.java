bfe0d2260e9f83042b5682feb75545e6be82d609
miw woo <mlolw2@naver.com>
2022-09-27 22:25:40 +0900

    Paging commit

--- blog/java/com/cos/blog/controller/BoardController.java
+++ blog/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.cos.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +18,8 @@ public class BoardController {
 	
 	
 	@GetMapping({"","/"})
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.글목록());
+	public String index(Model model, @PageableDefault(size=3,sort="id",direction=Sort.Direction.DESC)Pageable pageable) {
+		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index"; //viewResolver 작동!!
 	}
 	
