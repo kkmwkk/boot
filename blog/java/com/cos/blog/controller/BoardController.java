74119f83e4bc345cca4ac9a404a886481b6a7baa
miw woo <mlolw2@naver.com>
2022-09-27 22:41:51 +0900

    글 상세보기 Commit

--- blog/java/com/cos/blog/controller/BoardController.java
+++ blog/java/com/cos/blog/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.service.BoardService;
 
@@ -21,6 +22,13 @@ public class BoardController {
 	public String index(Model model, @PageableDefault(size=3,sort="id",direction=Sort.Direction.DESC)Pageable pageable) {
 		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index"; //viewResolver 작동!!
+	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/detail";
+		
 	}
 	
 	// USER 권한이 필요
--- blog/src/main/webapp/WEB-INF/views/index.jsp
+++ blog/src/main/webapp/WEB-INF/views/index.jsp
@@ -9,7 +9,7 @@
 		<img class="card-img-top">
 		<div class="card-body">
 			<h4 class="card-title">${board.title}</h4>
-			<a href="#" class="btn btn-primary">상세보기</a>
+			<a href="/board/${board.id}" class="btn btn-primary">상세보기</a>
 		</div>
 	</div>
 </c:forEach>
